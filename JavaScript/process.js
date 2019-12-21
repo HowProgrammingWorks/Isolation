3666044113a922aa10152547a923e600c8e71aab
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2019-12-22 00:07:29 +0200

    Pass requests to threads

--- JavaScript/process.js
+++ JavaScript/process.js
@@ -7,19 +7,36 @@ const { pid } = process;
 
 const BASE_PORT = 2000;
 
-const id = parseInt(process.argv[2], 10);
-const port = BASE_PORT + id - 1;
+const processId = parseInt(process.argv[2], 10);
+const port = BASE_PORT + processId - 1;
 
+const count = 3;
 const workers = [];
+let next = 0;
+let reqId = 0;
+const requests = new Map();
 
 const logs = [`  Process: ${pid}, port: ${port}`];
-for (let i = 0; i < 3; i++) {
+for (let i = 0; i < count; i++) {
   const worker = new Worker('./thread.js');
-  logs.push(`    Thread: ${pid}/${worker.threadId}`);
+  const tid = worker.threadId;
+  logs.push(`    Thread: ${pid}/${tid}`);
   workers.push(worker);
+  worker.on('message', ({ id, result }) => {
+    const context = requests.get(id);
+    requests.delete(id);
+    const { res } = context;
+    res.end(result);
+  });
 }
 console.log(logs.join('\n'));
 
 http.createServer((req, res) => {
-  res.end(`Process: ${pid}`);
+  const id = reqId++;
+  const { method, url } = req;
+  const worker = workers[next++];
+  if (next >= count) next = 0;
+  worker.postMessage({ id, method, url });
+  const context = { req, res, worker };
+  requests.set(id, context);
 }).listen(port);
--- JavaScript/thread.js
+++ JavaScript/thread.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const threads = require('worker_threads');
+const { pid } = process;
+const tid = threads.threadId;
 
-threads.parentPort.postMessage('Message from Worker to Master');
-
-threads.parentPort.on('message', data => {
-  console.dir({ data });
+threads.parentPort.on('message', ({ id, method, url }) => {
+  console.dir({ id, method, url });
+  const result = `Processed by ${pid}/${tid}`;
+  threads.parentPort.postMessage({ id, result });
 });
